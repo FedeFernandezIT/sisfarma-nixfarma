dc52a3b933dc2d04cb9fbc8e8a00321d7f453663
Federico Fernandez <fede.fernandez.it@gmail.com>
2020-06-07 23:37:47 +0200

    sincro ready

--- Sisfarma.Sincronizador.Unycop/Program.cs
+++ Sisfarma.Sincronizador.Unycop/Program.cs
@@ -80,7 +80,7 @@ namespace Sisfarma.Sincronizador.Unycop
             .AddSincronizador(new Domain.Core.Sincronizadores.ProductoCriticoSincronizador(
                     farmacia: FarmaciaFactory.Create(),
                     fisiotes: SisfarmaFactory.Create()),
-                    delay: 1000)//SincronizadorTaskManager.DelayProductosCriticos)
+                    delay: SincronizadorTaskManager.DelayProductosCriticos)
             .AddSincronizador(new Domain.Core.Sincronizadores.EncargoSincronizador(
                     farmacia: FarmaciaFactory.Create(),
                     fisiotes: SisfarmaFactory.Create()),
